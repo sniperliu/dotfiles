c16b8e397e5fcc6e4536aee8b1082c92f124e066
刘浩 <liuhao@basecity.com>
2018-08-14 11:50:00 +0800

    Add emacs clojure development configuration

---
+++ .emacs.d/init.el
@@ -0,0 +1,145 @@
+;;; init.el --- Summary
+;; Emacs configs for org & clojure development
+
+;;; Code:
+;; package management
+(require 'package)
+
+(setq package-enable-at-startup nil) ; Don't initialize later as well
+(setq package-check-signature nil)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+(setq package-user-dir (concat user-emacs-directory "vendors"))
+
+(package-initialize)
+
+;; use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+;; org mode
+(use-package org
+  :ensure org-plus-contrib)
+
+(load-library "url-handlers")
+
+(add-to-list 'load-path (format "%s/plugins" user-emacs-directory))
+
+;; don't let Customize mess with my .emacs
+;; (setq custom-file (concat "~/.emacs.d/custom.el"))
+;; (load custom-file 'noerror)
+
+;; Load private variables
+;; (when (file-exists-p (format "%s/private-variables.el" generated-basedir))
+;;   (load-file (format "%s/private-variables.el" generated-basedir)))
+
+;; Setup everything
+;; (org-babel-load-file (format "%s/main.org" user-emacs-directory))
+(defvar customizations-dir (format "%s/customizations" user-emacs-directory))
+(dolist (file '("ui.org"
+		"editing.org"
+		"navigation.org"
+                "system.org"
+                "setup-clojure.org"))
+  (org-babel-load-file (format "%s/%s" customizations-dir file)))
+
+(provide 'init)
+
+(put 'upcase-region 'disabled nil)
+(put 'magit-clean 'disabled nil)
+;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#657b83")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(custom-enabled-themes (quote (solarized-light)))
+ '(custom-safe-themes
+   (quote
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(fci-rule-color "#eee8d5")
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-symbol-colors
+   (--map
+    (solarized-color-blend it "#fdf6e3" 0.25)
+    (quote
+     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+ '(highlight-symbol-foreground-color "#586e75")
+ '(highlight-tail-colors
+   (quote
+    (("#eee8d5" . 0)
+     ("#B4C342" . 20)
+     ("#69CABF" . 30)
+     ("#69B7F0" . 50)
+     ("#DEB542" . 60)
+     ("#F2804F" . 70)
+     ("#F771AC" . 85)
+     ("#eee8d5" . 100))))
+ '(hl-bg-colors
+   (quote
+    ("#DEB542" "#F2804F" "#FF6E64" "#F771AC" "#9EA0E5" "#69B7F0" "#69CABF" "#B4C342")))
+ '(hl-fg-colors
+   (quote
+    ("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3")))
+ '(hl-paren-colors (quote ("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900")))
+ '(magit-diff-use-overlays nil)
+ '(nrepl-message-colors
+   (quote
+    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
+ '(package-selected-packages
+   (quote
+    (clj-refactor solarized-theme org-plus-contrib use-package)))
+ '(pos-tip-background-color "#eee8d5")
+ '(pos-tip-foreground-color "#586e75")
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
+ '(term-default-bg-color "#fdf6e3")
+ '(term-default-fg-color "#657b83")
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#dc322f")
+     (40 . "#c9485ddd1797")
+     (60 . "#bf7e73b30bcb")
+     (80 . "#b58900")
+     (100 . "#a5a58ee30000")
+     (120 . "#9d9d91910000")
+     (140 . "#9595943e0000")
+     (160 . "#8d8d96eb0000")
+     (180 . "#859900")
+     (200 . "#67119c4632dd")
+     (220 . "#57d79d9d4c4c")
+     (240 . "#489d9ef365ba")
+     (260 . "#3963a04a7f29")
+     (280 . "#2aa198")
+     (300 . "#288e98cbafe2")
+     (320 . "#27c19460bb87")
+     (340 . "#26f38ff5c72c")
+     (360 . "#268bd2"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (quote
+    (unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496")))
+ '(xterm-color-names
+   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#073642"])
+ '(xterm-color-names-bright
+   ["#fdf6e3" "#cb4b16" "#93a1a1" "#839496" "#657b83" "#6c71c4" "#586e75" "#002b36"]))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ README.md
@@ -0,0 +1,42 @@
+# Setup my MacOS anywhere
+
+Setup the MacOS everywhere, auto install the development tools mainly fish shell/iTerm/Emacs.
+
+## Prerequisite
+
+1. Git
+1. Ruby
+1. Homebrew
+
+```shell
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install git
+git clone https://github.com/sniperliu/dotfiles
+cd dotfiles
+./bootstrap.sh
+```
+
+## Installation
+
+## Settings
+
+### OS
+
+* 
+
+### Fish Shell
+
+### iTerm
+
+### Emacs
+
+### AWS
+
+## Reference
+
+1. https://github.com/atomantic/dotfiles
+1. https://github.com/seblemaguer/dotfiles
+1. https://github.com/avli/emacs-configs
+1. https://github.com/eigenhombre/emacs-config
+1. https://github.com/flyingmachine/emacs-for-clojure
+1. https://github.com/emacs-tw/awesome-emacs
