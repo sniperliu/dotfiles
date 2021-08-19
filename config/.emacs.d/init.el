157a10868b2e018168ae788fc330f2aa9d2c1926
LiuHao <sniperliuhao@gmail.com>
2021-08-19 21:51:06 +0800

    Fixed package install issue with enabling https for melpa

--- config/.emacs.d/init.el
+++ config/.emacs.d/init.el
@@ -9,9 +9,7 @@
 (setq package-enable-at-startup nil) ; Don't initialize later as well
 (setq package-check-signature nil)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 (setq package-user-dir (concat user-emacs-directory "vendors"))
 
@@ -61,12 +59,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("76c5b2592c62f6b48923c00f97f74bcb7ddb741618283bdb2be35f3c0e1030e3" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+   '("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "76c5b2592c62f6b48923c00f97f74bcb7ddb741618283bdb2be35f3c0e1030e3" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
  '(origami-show-fold-header t t)
  '(package-selected-packages
-   (quote
-    (smart-mode-line edts erlang erlang-mode zenburn-theme yasnippet-snippets yaml-mode which-key use-package-chords undo-tree toml-mode solarized-theme smex smart-mode-line-powerline-theme scala-mode sbt-mode sayid rainbow-delimiters quelpa-use-package plantuml-mode pdf-tools ox-reveal origami org-plus-contrib org-bullets markdown-preview-mode magit lsp-ui lsp-java intero interleave inf-clojure ido-vertical-mode htmlize hl-sexp helm-projectile helm-lsp helm-bibtex helm-ag gradle-mode git-gutter flycheck-rust flycheck-joker flx fill-column-indicator fic-mode expand-region exec-path-from-shell dockerfile-mode dap-mode counsel company-lsp clojure-snippets clj-refactor cargo beacon ag))))
+   '(helm parinfer-rust-mode vdiff-magit rust-mode with-editor expand-region magit counsel yasnippet-snippets nhexl-mode hexl-mode smart-mode-line edts erlang erlang-mode use-package-chords undo-tree toml-mode solarized-theme smex smart-mode-line-powerline-theme sayid rainbow-delimiters quelpa-use-package plantuml-mode pdf-tools ox-reveal origami org-bullets intero interleave ido-vertical-mode htmlize hl-sexp helm-projectile helm-bibtex gradle-mode flycheck-rust flycheck-joker flx fill-column-indicator fic-mode exec-path-from-shell dockerfile-mode dap-mode company-lsp clojure-snippets clj-refactor cargo beacon ag)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
